fb10942a60ea2a06ec03a4238a65a83337213e44
RameshXT <rameshxt.1@gmail.com>
2024-07-06 13:52:39 +0530

    Source

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public Message helloWorld() {
+        return new Message("Hello World");
+    }
+
+    public static class Message {
+        private String content;
+
+        public Message(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
+}
